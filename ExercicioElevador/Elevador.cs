39f9d3de6c55d84e61fb21dde41b80c71815bb7d
JoaoMonge <joao.monge13@gmail.com>
2022-05-25 10:57:06 +0100

    Initial commit

--- ExercicioElevador/Elevador.cs
+++ ExercicioElevador/Elevador.cs
@@ -1,6 +1,89 @@
 namespace ExercicioElevador;
 
+
+/*
+ * Crie uma classe denominada Elevador para armazenar as informações de um elevador dentro de um prédio.
+ * A classe deve armazenar o andar atual (actual = 0), total de andares no prédio (desconsiderando o RC), capacidade do elevador e quantas pessoas estão presentes nele.
+ *
+ * A classe deve também disponibilizar os seguintes métodos:
+
+Inicializa : que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio (os elevadores sempre começam no RC e vazio);
+
+Entra : para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço);
+
+Sai : para remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
+
+Sobe : para subir um andar (não deve subir se já estiver no último andar); Desce : para descer um andar (não deve descer se já estiver no RC); Encapsular todos os atributos da classe (criar os métodos set e get).
+ */
 public class Elevador
 {
+    private int actual = 0;
+    private int totalAndares;
+    private int capacidade;
+    private int pessoasDentro = 0;
+
+    public Elevador(int totalAndares, int capacidade)
+    {
+        this.totalAndares = totalAndares;
+        this.capacidade = capacidade;
+    }
+
+    public void entra()
+    {
+        if (capacidade > pessoasDentro)
+        {
+            pessoasDentro++;
+            Console.WriteLine("Entro uma pessoa.");
+
+        }
+        else
+        {
+            Console.WriteLine("O elevador está cheio.");
+        }
+    }
+
+    public void sai()
+    {
+        if (pessoasDentro > 0)
+        {
+            pessoasDentro--;
+            Console.WriteLine("Saiu uma pessoa");
+
+        }
+        else
+        {
+            Console.WriteLine("Elevador vazio");
+        }
+    }
+
+    public void sobe()
+    {
+        if (actual < totalAndares)
+        {
+            actual++;
+            Thread.Sleep(2000);
+            Console.WriteLine("{0}º Andar", actual);
+        }
+        else
+        {
+            Console.WriteLine("Falhou - Já se encontra no último piso");
+        }
+    }
+
+    public void desce()
+    {
+        if (actual > 0)
+        {
+            actual--;
+            Thread.Sleep(2000);
+            Console.WriteLine("{0}º Andar", actual);
+        }
+        else
+        {
+            Console.WriteLine("Falhou - Já se encontra no RC");
+
+        }
+    }
+    
     
 }
--- ExercicioElevador/Program.cs
+++ ExercicioElevador/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ExercicioElevador;
+
+Elevador e = new Elevador(4,4);
+
+e.entra();
+e.entra();
+
+e.sobe();
+e.sobe();  //2º Andar
+
+e.entra();
+e.entra();
+
+e.desce(); //1º 
+
+e.sobe();
+e.sobe();
+e.sobe(); //4º
+
+e.entra();
+e.entra();
+
+e.sai();
+e.sai();
+
+e.entra();
+
+e.desce();
+e.desce();
+e.desce();
+e.desce();
+e.desce();
